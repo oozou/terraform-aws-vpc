d305afebd09b5b943d92a48f738c9a12fa575f77
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-06-15 14:56:49 +0700

    (update): update s3 module version

--- modules/flow-log/kms.tf
+++ modules/flow-log/kms.tf
@@ -4,7 +4,7 @@
 module "flow_log_kms" {
   count = 1 - local.account_mode
 
-  source      = "git@github.com:oozou/terraform-aws-kms-key.git?ref=v0.0.2"
+  source      = "git@github.com:oozou/terraform-aws-kms-key.git?ref=v1.0.0"
   key_type    = "service"
   description = "Used to encrypt data for account centralize vpc flow log"
   prefix      = var.prefix
