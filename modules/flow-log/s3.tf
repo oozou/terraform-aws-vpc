885aae4b072e6e0afc953a56be3c7e5d3dcba710
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-06-15 15:08:00 +0700

    (update): s3 to version v1.0.4

--- modules/flow-log/s3.tf
+++ modules/flow-log/s3.tf
@@ -1,13 +1,13 @@
 module "centralize_flow_log_bucket" {
   count  = 1 - local.account_mode
-  source = "git@github.com:oozou/terraform-aws-s3?ref=v1.0.3"
+  source = "git@github.com:oozou/terraform-aws-s3?ref=v1.0.4"
 
   prefix      = var.prefix
   bucket_name = "account-flow-log"
   environment = "centralize"
 
   centralize_hub     = true
-  versioning_enabled = true
+  versioning_enabled = truet
   force_s3_destroy   = false
 
   is_enable_s3_hardening_policy = false
