808ccca15ca37f96286dc06547b4507ff4a21993
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-06-30 11:33:31 +0700

    (add): kms to cloudwatch log group and set defautl retention to 90 day

--- modules/flow-log/variables.tf
+++ modules/flow-log/variables.tf
@@ -37,8 +37,8 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "flow_log_retention_in_days" {
-  description = "Specifies the number of days you want to retain log events in the specified log group for VPC flow logs."
+variable "cloudwatch_log_retention_in_days" {
+  description = "Retention day for cloudwatch log group"
   type        = number
   default     = 90
 }
