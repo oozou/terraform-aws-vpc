e1865ebed9bf71a7eb23d10082968f6fb5a65a35
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-07-27 13:23:21 +0700

    (update): policy allow aws service to call kms

--- modules/flow-log/policies.tf
+++ modules/flow-log/policies.tf
@@ -58,6 +58,31 @@ data "aws_iam_policy_document" "kms_flow_log" {
       values   = ["arn:aws:logs:${data.aws_region.this.name}:*:log-group:/aws/vpc/${var.prefix}*"]
     }
   }
+
+  statement {
+    sid    = "Allow AWS Services to use the key"
+    effect = "Allow"
+    actions = [
+      "kms:GenerateDataKey*",
+      "kms:Decrypt",
+      "kms:List*",
+      "kms:DescribeKey*",
+      "kms:Encrypt",
+      "kms:ReEncrypt*"
+    ]
+    resources = ["*"]
+    principals {
+      identifiers = local.policy_identifiers
+      type        = "AWS"
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values = [
+        "s3.${data.aws_region.this.name}.amazonaws.com",
+      ]
+    }
+  }
 }
 
 data "aws_iam_policy_document" "s3_flow_log" {
