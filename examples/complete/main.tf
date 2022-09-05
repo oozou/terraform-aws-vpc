51e098724737da24d693cde03720457e74db8b18
Pongsak Sanguanwong <9073192+bemillenium@users.noreply.github.com>
2022-09-05 11:57:42 +0700

    chore: update 2nd cidr range

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -5,8 +5,8 @@ module "vpc" {
   account_mode = "hub"
 
   cidr              = "172.17.170.128/25"
-  secondary_cidr    = "100.64.0.0/16"
-  secondary_subnets = ["100.64.0.0/18", "100.64.64.0/18"]
+  secondary_cidr    = "100.64.0.0/22"
+  secondary_subnets = ["100.64.0.0/23", "100.64.2.0/23"]
   public_subnets    = ["172.17.170.192/28", "172.17.170.208/28"]
   private_subnets   = ["172.17.170.224/28", "172.17.170.240/28"]
   database_subnets  = ["172.17.170.128/27", "172.17.170.160/27"]
